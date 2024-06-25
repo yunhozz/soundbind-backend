3e0702d9720ce5b0e5f618b4b6216b8040640cbd
yunhozz <qkrdbsgh1121@naver.com>
2024-06-25 15:39:21 +0900

    CHORE: Configure Eureka Server Environment

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
