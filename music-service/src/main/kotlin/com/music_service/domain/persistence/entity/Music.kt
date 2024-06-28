047a352024d6c18b170edb7ada505fa29ee68754
yunhozz <qkrdbsgh1121@naver.com>
2024-06-29 04:44:53 +0900

    ADD: Update Fields on Music Entity #1
    - Genre Enum Class & Collection Table
    - 'deletedAt' for Soft Delete

--- music-service/src/main/kotlin/com/music_service/domain/persistence/entity/Music.kt
+++ music-service/src/main/kotlin/com/music_service/domain/persistence/entity/Music.kt
@@ -1,9 +1,15 @@
 package com.music_service.domain.persistence.entity
 
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
@@ -11,10 +17,12 @@ import java.time.LocalDateTime
 @Entity
 class Music(
     val userId: Long,
-    title: String,
     userNickname: String,
-    musicUrl: String,
+    title: String,
+    genres: Set<Genre> = hashSetOf(),
+    val musicUrl: String,
     imageUrl: String? = null,
+    val deletedAt: LocalDateTime? = null,
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,14 +34,24 @@ class Music(
     @LastModifiedDate
     var updatedAt: LocalDateTime? = null
 
-    var title: String = title
-        protected set
     var userNickname: String = userNickname
         protected set
-    var musicUrl: String = musicUrl
+
+    var title: String = title
         protected set
+
+    @ElementCollection(fetch = FetchType.LAZY, targetClass = Genre::class)
+    @CollectionTable(joinColumns = [JoinColumn(name = "music_id")], name = "music_genre")
+    @Enumerated(EnumType.STRING)
+    var genres: Set<Genre> = genres
+        protected set
+
     var imageUrl: String? = imageUrl
         protected set
+
+    fun updateGenres(genres: Set<Genre>) {
+        this.genres = genres
+    }
 
     fun updateTitle(title: String) {
         this.title = title
@@ -41,5 +59,27 @@ class Music(
 
     fun updateImageUrl(url: String) {
         this.imageUrl = url
+    }
+
+    enum class Genre(
+        val genreName: String
+    ) {
+        CLASSIC("클래식"),
+        JAZZ("재즈"),
+        POP("팝"),
+        BALLAD("발라드"),
+        HIPHOP("힙합"),
+        COUNTRY("컨트리"),
+        DISCO("디스코"),
+        ROCK("락"),
+        ELECTRONIC("일렉트로닉스"),
+        TROT("트로트")
+        ;
+
+        companion object {
+            fun of(genreName: String): Genre = entries.find {
+                it.name == genreName
+            } ?: throw IllegalArgumentException("Unknown genre '$genreName'")
+        }
     }
 }
