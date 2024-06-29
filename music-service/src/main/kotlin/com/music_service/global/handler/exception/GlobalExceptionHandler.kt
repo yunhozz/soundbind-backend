423126cd638ed3272405140598810f96fcc689d2
yunhozz <qkrdbsgh1121@naver.com>
2024-06-29 18:22:41 +0900

    ADD: Create Global Exception Handler #1

---
+++ music-service/src/main/kotlin/com/music_service/global/handler/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,37 @@
+package com.music_service.global.handler.exception
+
+import com.music_service.global.dto.response.ErrorResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(RuntimeException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleRuntimeException(e: RuntimeException): ErrorResponse {
+        log.error(e.stackTraceToString())
+        return ErrorResponse.of(ErrorResponse.ErrorCode.INTERNAL_SERVER_ERROR, e.localizedMessage)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ErrorResponse {
+        log.error(e.localizedMessage)
+        return ErrorResponse.of(ErrorResponse.ErrorCode.INVALID_REQUEST, e.bindingResult)
+    }
+
+    @ExceptionHandler(HttpClientErrorException::class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    fun handleHttpClientErrorException(e: HttpClientErrorException): ErrorResponse {
+        log.error(e.localizedMessage)
+        return ErrorResponse.of(ErrorResponse.ErrorCode.METHOD_NOT_ALLOWED, e.responseBodyAsString)
+    }
+}
