c6ffa67611ef34d6d743f21218bcb53f376fe5cb
yunhozz <qkrdbsgh1121@naver.com>
2024-07-01 23:26:26 +0900

    CHORE: Add Dependency of QueryDSL #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	kotlin("jvm") version "2.0.0"
 	kotlin("plugin.jpa") version "2.0.0"
 	kotlin("plugin.spring") version "2.0.0"
+	kotlin("kapt") version "1.8.10"
 	id("org.springframework.boot") version "3.3.1"
 	id("io.spring.dependency-management") version "1.1.5"
 }
@@ -23,6 +24,7 @@ subprojects {
 	apply(plugin = "kotlin-spring")
 	apply(plugin = "kotlin-allopen")
 	apply(plugin = "kotlin-noarg")
+	apply(plugin = "kotlin-kapt")
 	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
 
--- music-service/build.gradle.kts
+++ music-service/build.gradle.kts
@@ -6,8 +6,16 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("com.querydsl:querydsl-jpa:5.1.0")
+	implementation("com.querydsl:querydsl-apt:5.1.0:jpa")
+	kapt("com.querydsl:querydsl-apt:5.1.0:jpa")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<JavaCompile> {
+	options.annotationProcessorPath = configurations.kapt.get()
 }
