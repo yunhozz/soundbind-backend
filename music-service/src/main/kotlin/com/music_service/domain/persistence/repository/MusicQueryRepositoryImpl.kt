c2d904dcf3d1b0fe1f912541e6814a8c4bc98873
yunhozz <qkrdbsgh1121@naver.com>
2024-07-04 23:40:54 +0900

    UPDATE: 음원 상세 조회 시 내용 보완 #1

--- music-service/src/main/kotlin/com/music_service/domain/persistence/repository/MusicQueryRepositoryImpl.kt
+++ music-service/src/main/kotlin/com/music_service/domain/persistence/repository/MusicQueryRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.music_service.domain.persistence.repository
 import com.music_service.domain.persistence.entity.QFileEntity.fileEntity
 import com.music_service.domain.persistence.entity.QMusic.music
 import com.music_service.global.dto.response.MusicDetailsQueryDTO
-import com.music_service.global.dto.response.QMusicDetailsQueryDTO
 import com.music_service.global.dto.response.QMusicDetailsQueryDTO_MusicFileQueryDTO
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
@@ -14,20 +13,12 @@ class MusicQueryRepositoryImpl(
 ): MusicQueryRepository {
 
     override fun findMusicDetailsById(id: Long): MusicDetailsQueryDTO? {
-        val musicDetails = queryFactory
-            .select(
-                QMusicDetailsQueryDTO(
-                    music.id,
-                    music.userId,
-                    music.userNickname,
-                    music.title
-                )
-            )
-            .from(music)
+        val m = queryFactory
+            .selectFrom(music)
             .where(music.id.eq(id))
             .fetchOne()
 
-        return musicDetails?.let {
+        return m?.let {
             val musicFiles = queryFactory
                 .select(
                     QMusicDetailsQueryDTO_MusicFileQueryDTO(
@@ -39,11 +30,20 @@ class MusicQueryRepositoryImpl(
                 )
                 .from(fileEntity)
                 .join(fileEntity.music, music)
-                .where(music.id.eq(musicDetails.id))
+                .where(music.id.eq(m.id))
                 .fetch()
 
-            musicDetails.files = musicFiles
-            return musicDetails
+            val musicDetailsQueryDTO = MusicDetailsQueryDTO(
+                m.id!!,
+                m.userId,
+                m.userNickname,
+                m.title,
+                m.genres,
+                m.createdAt,
+                m.updatedAt
+            )
+            musicDetailsQueryDTO.files = musicFiles
+            return musicDetailsQueryDTO
         }
     }
 }
--- music-service/src/main/kotlin/com/music_service/global/dto/response/MusicDetailsQueryDTO.kt
+++ music-service/src/main/kotlin/com/music_service/global/dto/response/MusicDetailsQueryDTO.kt
@@ -1,12 +1,17 @@
 package com.music_service.global.dto.response
 
+import com.music_service.domain.persistence.entity.Music
 import com.querydsl.core.annotations.QueryProjection
+import java.time.LocalDateTime
 
-data class MusicDetailsQueryDTO @QueryProjection constructor(
+data class MusicDetailsQueryDTO(
     val id: Long,
     val userId: Long,
     val userNickname: String,
-    val title: String
+    val title: String,
+    val genres: Set<Music.Genre>,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
 ) {
     lateinit var files: List<MusicFileQueryDTO>
 
