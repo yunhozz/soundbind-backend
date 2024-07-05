804f7f1487bc126e354c352fd0e66fee265898e0
yunhozz <qkrdbsgh1121@naver.com>
2024-07-05 22:12:16 +0900

    REFACTOR: 음원 및 이미지 Resource Handler 상속 구조 변경 #1

--- music-service/src/main/kotlin/com/music_service/domain/application/file/ResourceHandler.kt
+++ music-service/src/main/kotlin/com/music_service/domain/application/file/ResourceHandler.kt
@@ -1,38 +1,39 @@
 package com.music_service.domain.application.file
 
+import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
+import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.UUID
 
 sealed interface ResourceHandler {
     companion object {
+        private val ABSOLUTE_PATH: String = File("").absolutePath
         private const val ROOT_DIRECTORY = "/music-service/src/main/resources/file/"
-        private val ABSOLUTE_PATH = File("").absolutePath
         private val CURRENT_DATE = SimpleDateFormat("yy-MM-dd").format(Date())
         private val FILE_DIRECTORY = ABSOLUTE_PATH + ROOT_DIRECTORY + CURRENT_DATE
     }
 
-    var originalFileName: String
-    var savedName: String
-    var fileUrl: String
-
-    @Throws(IOException::class)
-    fun upload(file: MultipartFile?) {
+    fun upload(file: MultipartFile?): Triple<String, String, String> {
         file?.let {
-            originalFileName = file.originalFilename!!
+            val originalFileName = file.originalFilename!!
             val extension = originalFileName.substring(originalFileName.lastIndexOf("."))
             val uuid = UUID.randomUUID().toString()
-            savedName = uuid + extension
-            fileUrl = "$CURRENT_DATE/$savedName"
+            val savedName = uuid + extension
+            val fileUrl = "$CURRENT_DATE/$savedName"
 
             File("$FILE_DIRECTORY/$savedName").also {
                 if (!it.exists()) it.mkdirs()
                 file.transferTo(it)
             }
+            return Triple(savedName, uuid, fileUrl)
+
         } ?: throw IOException("File does not exist.")
     }
 
@@ -43,34 +44,19 @@ sealed interface ResourceHandler {
                 throw IOException("Failed to delete file: $fileUrl")
         }
         else
-            throw IllegalArgumentException("File does not exist: $fileUrl")
+            throw IOException("File does not exist: $fileUrl")
     }
 }
 
-interface MusicResourceHandler: ResourceHandler {
-    @Throws(IOException::class)
-    fun uploadMusic(file: MultipartFile): Triple<String, String, String> {
-        upload(file)
-        return Triple(originalFileName, savedName, fileUrl)
-    }
-
-    @Throws(IOException::class)
-    fun downloadMusic(fileUrl: String): Resource
-
-    @Throws(IOException::class)
+@Component
+class MusicResourceHandler: ResourceHandler {
+    fun uploadMusic(file: MultipartFile): Triple<String, String, String> = upload(file)
+    fun downloadMusic(fileUrl: String): Pair<Resource, String> = download(fileUrl)
     fun deleteMusic(fileUrl: String) = delete(fileUrl)
 }
 
-interface ImageResourceHandler: ResourceHandler {
-    @Throws(IOException::class)
-    fun uploadImage(file: MultipartFile): Triple<String, String, String> {
-        upload(file)
-        return Triple(originalFileName, savedName, fileUrl)
-    }
-
-    @Throws(IOException::class)
-    fun displayImage(fileUrl: String): Resource
-
-    @Throws(IOException::class)
+@Component
+class ImageResourceHandler: ResourceHandler {
+    fun uploadImage(file: MultipartFile): Triple<String, String, String> = upload(file)
     fun deleteImage(fileUrl: String) = delete(fileUrl)
 }
