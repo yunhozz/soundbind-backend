55b260944be2844db8caef9effabfcd665ca0bff
yunhozz <qkrdbsgh1121@naver.com>
2024-07-09 21:38:42 +0900

    CHORE: Querydsl 의존성 및 설정 추가 #4

--- review-service/build.gradle.kts
+++ review-service/build.gradle.kts
@@ -7,6 +7,18 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.kafka:spring-kafka")
+
+	// querydsl
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+	implementation("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("jakarta.annotation:jakarta.annotation-api")
+	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	// query logging
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
