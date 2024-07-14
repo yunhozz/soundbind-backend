16095f3bc0ec851a33b61c191f377e847153935b
yunhozz <qkrdbsgh1121@naver.com>
2024-07-14 19:51:52 +0900

    CHORE: 도커라이징을 위한 환경 수정

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
