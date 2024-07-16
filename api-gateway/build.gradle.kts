a609aff3ecb1c901abce9a291a620a643f1f0fc2
yunhozz <qkrdbsgh1121@naver.com>
2024-07-16 16:08:35 +0900

    CHORE: API Gateway 및 Config Server를 Eureka Server에서 분리

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("io.projectreactor:reactor-test")
--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-config-server")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
