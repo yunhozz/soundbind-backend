bcd7d8063e874c3f2026afaa15e101c2c35a42a6
yunhozz <qkrdbsgh1121@naver.com>
2024-07-21 18:05:18 +0900

    ADD: API Response DTO 및 전역 예외 핸들러 추가 #9

---
+++ auth-service/src/main/kotlin/com/auth_service/domain/interfaces/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,45 @@
+package com.auth_service.domain.interfaces.handler
+
+import com.auth_service.domain.interfaces.dto.ErrorResponse
+import com.auth_service.domain.interfaces.dto.ErrorResponse.ErrorCode
+import org.slf4j.LoggerFactory
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException
+
+@RestControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
+class GlobalExceptionHandler {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
+        log.error(e.stackTraceToString())
+        return ResponseEntity
+            .status(500)
+            .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e.localizedMessage))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        log.error(e.localizedMessage)
+        return ResponseEntity
+            .status(e.statusCode)
+            .body(ErrorResponse.of(ErrorCode.BAD_REQUEST, e.bindingResult))
+    }
+
+    @ExceptionHandler(HttpClientErrorException::class)
+    fun handleHttpClientErrorException(e: HttpClientErrorException): ResponseEntity<ErrorResponse> {
+        log.error(e.localizedMessage)
+        return ResponseEntity
+            .status(e.statusCode)
+            .body(ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED, e.responseBodyAsString))
+    }
+}
