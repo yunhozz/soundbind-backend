e3d15f7776807731bb2b39c4c7a955cd9639a454
yunhozz <qkrdbsgh1121@naver.com>
2024-07-26 02:06:34 +0900

    CHORE: jackson 의존성 추가 #3

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
