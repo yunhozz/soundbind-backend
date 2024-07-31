af6e919e327a2d29ca8e97ad400b6334b1df43fb
yunhozz <qkrdbsgh1121@naver.com>
2024-07-31 17:39:00 +0900

    CHORE: querydsl 및 로깅 관련 의존성 추가 #9

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -6,14 +6,30 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// jackson
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// jjwt
 	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	// querydsl
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+	implementation("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("jakarta.annotation:jakarta.annotation-api")
+	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	// query logging
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
