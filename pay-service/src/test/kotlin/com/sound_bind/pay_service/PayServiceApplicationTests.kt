cacd901ed41dc49f5088639d5b104d89b5a9f740
yunhozz <qkrdbsgh1121@naver.com>
2024-08-02 10:53:37 +0900

    [#11] Initial Commit

---
+++ pay-service/src/test/kotlin/com/sound_bind/pay_service/PayServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sound_bind.pay_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
