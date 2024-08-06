fd54fb3156ccb27ad14ab7eaf7b48d2c66364bc6
yunhozz <qkrdbsgh1121@naver.com>
2024-08-06 15:43:42 +0900

    [#12] CHORE: 음원 서비스에 kafka 의존성 추가

--- music-service/build.gradle.kts
+++ music-service/build.gradle.kts
@@ -6,14 +6,22 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.kafka:spring-kafka")
+
+	// querydsl
 	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
 	implementation("com.querydsl:querydsl-apt:5.1.0:jakarta")
 	implementation("jakarta.persistence:jakarta.persistence-api")
 	implementation("jakarta.annotation:jakarta.annotation-api")
 	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	// query logging
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
