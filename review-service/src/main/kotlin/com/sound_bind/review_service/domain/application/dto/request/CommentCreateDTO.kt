6e15b3c786a0534ade5f7e2920ef4832f1d57f33
yunhozz <qkrdbsgh1121@naver.com>
2024-08-17 10:36:05 +0900

    [#13] UPDATE: 리뷰 댓글 생성 알림 로직 추가

--- review-service/src/main/kotlin/com/sound_bind/review_service/domain/application/dto/request/CommentCreateDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.sound_bind.review_service.domain.application.dto.request
-
-import jakarta.validation.constraints.NotBlank
-
-data class CommentCreateDTO(
-    @field:NotBlank
-    val message: String
-)
