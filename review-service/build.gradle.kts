b140a6ccd568504afc9ca04d149c4390357d298c
yunhozz <qkrdbsgh1121@naver.com>
2024-08-21 19:51:56 +0900

    [#16] CHORE: Elasticsearch 의존성 추가

--- review-service/build.gradle.kts
+++ review-service/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
