d3502434c87729cfc8d826d3a2ca96ea6b6152ec
yunhozz <qkrdbsgh1121@naver.com>
2024-09-11 18:25:13 +0900

    [#19] TEST: 분산락 적용에 의한 테스트 코드 수정

--- auth-service/src/test/kotlin/com/auth_service/domain/application/UserManageServiceTest.kt
+++ auth-service/src/test/kotlin/com/auth_service/domain/application/UserManageServiceTest.kt
@@ -2,13 +2,13 @@ package com.auth_service.domain.application
 
 import com.auth_service.domain.application.dto.request.SignUpRequestDTO
 import com.auth_service.domain.persistence.repository.UserProfileRepository
-import com.auth_service.global.exception.UserManageException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.CannotAcquireLockException
 import java.time.Duration
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.CountDownLatch
@@ -24,7 +24,7 @@ class UserManageServiceTest {
     private lateinit var userProfileRepository: UserProfileRepository
 
     @Test
-    fun `Test Concurrency in createLocalUser() with 1000 Threads`() {
+    fun `Test Concurrency in createLocalUser() with 5 Threads`() {
         // given
         val startTime = System.nanoTime()
         val dto = SignUpRequestDTO(
@@ -35,7 +35,7 @@ class UserManageServiceTest {
             profileUrl = "test-image.jpg"
         )
 
-        val threads = 1000
+        val threads = 5
         val executorService = Executors.newFixedThreadPool(threads)
         val latch = CountDownLatch(threads)
 
@@ -64,7 +64,7 @@ class UserManageServiceTest {
         // then
         assertEquals(1, successResults.size, "1개의 회원가입만 성공해야 합니다.")
         assertEquals(threads - 1, failureResults.size, "${threads - 1}개의 회원가입은 실패해야 합니다.")
-        assertTrue(failureResults.all { it is UserManageException.EmailDuplicateException }, "발생한 예외는 모두 EmailDuplicateException 이어야 합니다.")
+        assertTrue(failureResults.all { it is CannotAcquireLockException }, "발생한 예외는 모두 CannotAcquireLockException 이어야 합니다.")
         assertNotNull(userProfileRepository.findWithUserByUserId(successResults.first()), "회원가입이 완료된 회원 조회에 성공해야 합니다.")
 
         println("테스트 실행 시간 : ${duration.toMillis()} 밀리초")
