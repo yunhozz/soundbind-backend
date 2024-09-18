d39235f5b23022b47649e7723e99da487ca92fd2
yunhozz <qkrdbsgh1121@naver.com>
2024-09-18 21:49:57 +0900

    [#19] TEST: 좋아요 동시성 테스트 추가

--- music-service/src/test/kotlin/com/music_service/domain/application/MusicServiceConcurrencyTest.kt
+++ music-service/src/test/kotlin/com/music_service/domain/application/MusicServiceConcurrencyTest.kt
@@ -1,14 +1,21 @@
 package com.music_service.domain.application
 
 import com.music_service.domain.application.dto.request.MusicCreateDTO
+import com.music_service.domain.application.dto.request.MusicUpdateDTO
+import com.music_service.domain.application.manager.LockManager
+import com.music_service.domain.persistence.repository.MusicLikesRepository
 import com.music_service.domain.persistence.repository.MusicRepository
 import com.music_service.global.util.RedisUtils
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockMultipartFile
 import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
@@ -23,24 +30,16 @@ class MusicServiceConcurrencyTest {
     @Autowired
     private lateinit var musicRepository: MusicRepository
 
-    @Test
-    fun `Test Concurrency in uploadMusic() with 1000 Threads`() {
-        // given
-        val startTime = System.currentTimeMillis()
+    @Autowired
+    private lateinit var musicLikesRepository: MusicLikesRepository
 
-        val musicFile = MockMultipartFile("file", "music-file.mp4", "audio/mp4", "This is Music".toByteArray())
-        val imageFile = MockMultipartFile("file", "image-file.jpg", "image/jpeg", "This is Image".toByteArray())
-        val musicCreateDTO = MusicCreateDTO(
-            title = "Test Music",
-            genres = setOf("POP", "ROCK"),
-            musicFile = musicFile,
-            imageFile = imageFile
-        )
+    @Autowired
+    private lateinit var lockManager: LockManager
 
-        val threads = 1000
-        val executorService = Executors.newFixedThreadPool(threads)
-        val latch = CountDownLatch(threads)
+    private val threads = 500
 
+    @BeforeEach
+    fun initUserInfoOnRedis() {
         (1..threads).toList()
             .map { userId ->
                 mapOf(
@@ -52,11 +51,37 @@ class MusicServiceConcurrencyTest {
             }.forEach { userInfo ->
                 RedisUtils.saveJson("user:${userInfo["userId"]}", userInfo, Duration.ofSeconds(30))
             }
+    }
+
+    @AfterEach
+    fun cleanupRedis() {
+        (1..threads).toList()
+            .forEach { userId ->
+                RedisUtils.deleteValue("user:$userId")
+            }
+    }
+
+    @Test
+    fun `Test Concurrency in uploadMusic() with 1000 Users`() {
+        // given
+        val musicFile = MockMultipartFile("file", "music-file.mp4", "audio/mp4", "This is Music".toByteArray())
+        val imageFile = MockMultipartFile("file", "image-file.jpg", "image/jpeg", "This is Image".toByteArray())
+        val musicCreateDTO = MusicCreateDTO(
+            title = "Test Music",
+            genres = setOf("POP", "ROCK"),
+            musicFile = musicFile,
+            imageFile = imageFile
+        )
+
+        val executorService = Executors.newFixedThreadPool(threads)
+        val latch = CountDownLatch(threads)
 
         val successResults = CopyOnWriteArrayList<Long>()
         val failureResults = CopyOnWriteArrayList<Throwable>()
 
         // when
+        val startTime = System.currentTimeMillis()
+
         repeat(threads) {
             executorService.execute {
                 val userId = ThreadLocalRandom.current().nextLong(1, threads.toLong())
@@ -75,12 +100,127 @@ class MusicServiceConcurrencyTest {
         executorService.shutdown()
         latch.await()
 
-        val endTime = System.currentTimeMillis()
-
         // then
-        println("테스트 실행 시간 : ${endTime - startTime} 밀리초")
+        println("테스트 실행 시간 : ${System.currentTimeMillis() - startTime} 밀리초")
 
         assertEquals(threads, successResults.size, "$threads 개의 음원 업로드에 모두 성공해야 합니다.")
         assertEquals(0, failureResults.size, "음원 업로드에 실패한 thread 는 없어야 합니다.")
+    }
+
+    @Test
+    fun `Test Concurrency in updateMusicInformation() with 1000 Users`() {
+        // given
+        val musicFile = MockMultipartFile("file", "music-file.mp4", "audio/mp4", "This is Music".toByteArray())
+        val imageFile = MockMultipartFile("file", "image-file.jpg", "image/jpeg", "This is Image".toByteArray())
+
+        val userIds = (1L..threads).toList()
+        for (userId in userIds) {
+            val musicCreateDTO = MusicCreateDTO(
+                title = "Test Music $userId",
+                genres = setOf("POP", "ROCK"),
+                musicFile = musicFile,
+                imageFile = imageFile
+            )
+            musicService.uploadMusic(userId, musicCreateDTO)
+            Thread.sleep(10)
+        }
+
+        val executorService = Executors.newFixedThreadPool(threads)
+        val latch = CountDownLatch(threads)
+
+        val successResults = ConcurrentHashMap<Int, Long>()
+        val failureResults = CopyOnWriteArrayList<Throwable>()
+
+        // when
+        val startTime = System.currentTimeMillis()
+
+        repeat(threads) { index ->
+            executorService.execute {
+                try {
+                    val musicUpdateDTO = MusicUpdateDTO(
+                        title = "Music Updated $index",
+                        genres = setOf("BALLAD"),
+                        imageFile = MockMultipartFile(
+                            "file",
+                            "update-file-$index.jpg",
+                            "image/jpeg",
+                            "This is Updated".toByteArray()
+                        )
+                    )
+                    val result = musicService.updateMusicInformation(index.toLong(), musicUpdateDTO)
+                    successResults[index] = result
+
+                } catch (e: Exception) {
+                    failureResults.add(e)
+
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+        executorService.shutdown()
+        latch.await()
+
+        // then
+        println("테스트 실행 시간 : ${System.currentTimeMillis() - startTime} 밀리초")
+
+        assertEquals(threads, successResults.size, "$threads 개의 음원 업데이트에 모두 성공해야 합니다.")
+        assertEquals(0, failureResults.size, "음원 업데이트에 실패한 thread 는 없어야 합니다.")
+        assertTrue(musicRepository.findAll().all { it.title.startsWith("Music Updated") }, "모든 음원이 정상적으로 업데이트 되어야 합니다.")
+        assertTrue(successResults.keys.toSet().size == threads, "업데이트가 겹치지 않아야 합니다.")
+    }
+
+    @Test
+    fun `Test Concurrency in changeLikesFlag() with 1000 Users`() {
+        // given
+        val userId = threads.toLong()
+
+        val musicFile = MockMultipartFile("file", "music-file.mp4", "audio/mp4", "This is Music".toByteArray())
+        val imageFile = MockMultipartFile("file", "image-file.jpg", "image/jpeg", "This is Image".toByteArray())
+        val musicCreateDTO = MusicCreateDTO(
+            title = "Test Music",
+            genres = setOf("POP", "ROCK"),
+            musicFile = musicFile,
+            imageFile = imageFile
+        )
+        val musicId = musicService.uploadMusic(userId, musicCreateDTO)
+
+        val executorService = Executors.newFixedThreadPool(threads)
+        val latch = CountDownLatch(threads)
+
+        val successResults = CopyOnWriteArrayList<Long>()
+        val failureResults = CopyOnWriteArrayList<Throwable>()
+
+        // when
+        val startTime = System.currentTimeMillis()
+
+        repeat(threads) { thread ->
+            executorService.execute {
+                try {
+                    val randomUserId = ThreadLocalRandom.current().nextLong(1, threads.toLong())
+                    val result = musicService.changeLikesFlag(musicId, thread.toLong())
+                    result?.let { successResults.add(it) }
+
+                } catch (e: Exception) {
+                    failureResults.add(e)
+
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+        executorService.shutdown()
+        latch.await()
+
+        // then
+        failureResults.forEach { println(it) }
+        println("테스트 실행 시간 : ${System.currentTimeMillis() - startTime} 밀리초")
+
+        assertEquals(
+            musicRepository.findById(musicId).get().likes,
+            musicLikesRepository.findMusicLikesByFlag(true).size,
+            "무작위 $threads 명의 음원 좋아요에 모두 성공해야 합니다."
+        )
+        assertEquals(0, failureResults.size, "음원 좋아요에 실패한 thread 는 없어야 합니다.")
     }
 }
