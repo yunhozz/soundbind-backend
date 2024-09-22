6bdcdac218038517d9d4c264eb07a69731e3b183
yunhozz <qkrdbsgh1121@naver.com>
2024-09-23 01:57:40 +0900

    [#22] ADD: Swagger UI 설정 추가

---
+++ auth-service/src/main/kotlin/com/auth_service/global/config/SwaggerConfig.kt
@@ -0,0 +1,39 @@
+package com.auth_service.global.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@OpenAPIDefinition(
+    info = Info(
+        title = "Auth Service API",
+        description = "Sound Bind 인증 서비스 API 입니다."
+    )
+)
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val jwt = "JWT"
+        val securityRequirement = SecurityRequirement().addList(jwt)
+        val components = Components().addSecuritySchemes(jwt,
+            SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat(jwt)
+        )
+
+        return OpenAPI()
+            .addServersItem(Server().url("http://localhost:8000"))
+            .addSecurityItem(securityRequirement)
+            .components(components)
+    }
+}
