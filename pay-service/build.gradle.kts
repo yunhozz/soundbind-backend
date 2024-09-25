78b6a2b3d8db35e00f5a146c478e10d42bf8709d
yunhozz <qkrdbsgh1121@naver.com>
2024-09-26 00:55:13 +0900

    [#11] CHORE: Redisson, Swagger 관련 의존성 추가

--- pay-service/build.gradle.kts
+++ pay-service/build.gradle.kts
@@ -2,11 +2,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.kafka:spring-kafka")
+
+	// kotlin reflect
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
 	// querydsl
 	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
@@ -16,8 +18,17 @@ dependencies {
 	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 
+	// redisson
+	implementation("org.redisson:redisson-spring-boot-starter:3.27.0")
+
+	// jackson
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	// query logging
 	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
+	// swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
