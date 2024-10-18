8ed4827eaf73fb03ee2d19f02a50989c7e8b2944
yunhozz <qkrdbsgh1121@naver.com>
2024-10-18 20:02:49 +0900

    [#27] UPDATE: Kafka Producer를 Cloud Function으로 전환

--- kafka-server/src/main/kotlin/com/sound_bind/kafka_server/domain/service/KafkaService.kt
+++ kafka-server/src/main/kotlin/com/sound_bind/kafka_server/domain/service/KafkaService.kt
@@ -1,44 +1,38 @@
-package com.sound_bind.kafka_server.domain.controller
+package com.sound_bind.kafka_server.domain.service
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.sound_bind.global.utils.logger
-import io.swagger.v3.oas.annotations.Operation
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Service
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 
-@RestController
-@RequestMapping("/api/kafka")
-class KafkaController(
-    private val kafkaTemplate: KafkaTemplate<String, Map<String, Any>>
+@Service
+class KafkaService(
+    private val kafkaTemplate: KafkaTemplate<String, Map<String, Any>>,
 ) {
-
-    private val logger = logger()
     private val mapper = jacksonObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-    @PostMapping
-    @Operation(summary = "kafka topic 발행")
-    fun sendKafkaMessage(@RequestBody data: String) {
-        val response = if (data.trim().startsWith("[")) {
-            mapper.readValue(data, List::class.java)
+    private val logger = logger()
+
+    fun publishEvent(): (String) -> Unit = { event ->
+        val data = if (event.trim().startsWith("[")) {
+            mapper.readValue(event, List::class.java)
         } else {
-            listOf(mapper.readValue(data, Map::class.java))
+            listOf(mapper.readValue(event, Map::class.java))
         }
-        logger.info("Received Data : $response")
-        val threads = response.size
+        logger.info("Received Data : $data")
+
+        val threads = data.size
         val executorService = Executors.newFixedThreadPool(threads)
         val latch = CountDownLatch(threads)
 
         executorService.execute {
-            for (r in response) {
-                val payload = mapper.readValue(mapper.writeValueAsString(r), Map::class.java)
+            for (d in data) {
+                val payload = mapper.readValue(mapper.writeValueAsString(d), Map::class.java)
                 val topic = payload["topic"].toString()
                 val message = payload["message"]
 
--- kafka-server/src/main/kotlin/com/sound_bind/kafka_server/global/config/CloudFunctionConfig.kt
+++ kafka-server/src/main/kotlin/com/sound_bind/kafka_server/global/config/CloudFunctionConfig.kt
@@ -2,15 +2,19 @@ package com.sound_bind.kafka_server.global.config
 
 import com.sound_bind.global.dto.OrchestrationRequestDTO
 import com.sound_bind.global.dto.OrchestrationResponseDTO
+import com.sound_bind.kafka_server.domain.service.KafkaService
 import com.sound_bind.kafka_server.domain.service.OrchestrationService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
 
 @Configuration
-class OrchestrationConfig(
+class CloudFunctionConfig(
+    private val kafkaService: KafkaService,
     private val orchestrationService: OrchestrationService
 ) {
+    @Bean
+    fun kafka(): (String) -> Unit = kafkaService.publishEvent()
 
     @Bean
     fun process(): (Flux<OrchestrationRequestDTO>) -> Flux<OrchestrationResponseDTO> =
